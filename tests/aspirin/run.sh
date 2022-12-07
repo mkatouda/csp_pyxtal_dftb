e498a53beb6feee9d4729611fb6668d051618fe4
Michio Katouda <katouda@rist.or.jp>
2022-12-07 15:17:13 +0900

    Bug fix

--- tests/aspirin/run.sh
+++ tests/aspirin/run.sh
@@ -10,4 +10,4 @@ export MKL_NUM_THREADS=${NUM_THREADS}
 export OMP_NUM_THREADS=${NUM_THREADS},1
 export OMP_STACKSIZE=4G
 
-csp_pyxtal_dftb -i ${in_file} >& ${out_file}
+csp_pyxtal -i ${in_file} >& ${out_file}
